a7c4fe245ec91807e9f2900be55ee1aac93dea3e
Vaishnav babu <Vaishnavbabu93@gmail.com>
2023-07-09 12:54:12 +0400

    Added download button

--- app.py
+++ app.py
@@ -29,6 +29,14 @@ hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 st.bar_chart(hist_values)
 
 
+st.download_button(
+    label="Download data as CSV",
+    data=csv,
+    file_name='large_df.csv',
+    mime='text/csv',
+)
+
+
 # Some number in the range 0-23
 hour_to_filter = st.slider('hour', 0, 23, 17)
 filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
