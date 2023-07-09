b9a196746fd6df8dece6519a7c65efa902750526
Vaishnav babu <Vaishnavbabu93@gmail.com>
2023-07-09 13:19:24 +0400

    pg icon

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ import numpy as np
 
 st.title('Web Map')
 
+st.set_page_config(
+    page_icon="https://www.pngegg.com/en/search?q=globe+Icon"
+)
+
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
             'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
